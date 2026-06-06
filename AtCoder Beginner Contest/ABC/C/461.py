10410b00f9e2db38445ff3dc611d29bccafa9b84
gonchiman <legendaryyamasama@gmail.com>
2026-06-06 22:41:08 +0900

    s

--- AtCoder Beginner Contest/ABC/C/461.py
+++ AtCoder Beginner Contest/ABC/C/461.py
@@ -1,30 +1,59 @@
 N, K, M = map(int, input().split())
-C_V = [list(map(int, input().split())) for _ in range(N)]
 
+class Jewelry:
+    def __init__(self, c, v, i):
+        self.category = c
+        self.value = v
+        self.index = i
 
+C_V = list()
+
+for j in range(N):
+    c, v = map(int, input().split())
+    C_V.append(Jewelry(c, v, j))
+
+# {category: [index, value]}
 max_values = {}
 
-# init max_values
-for i in range(N):
-    if C_V[i][0] not in max_values.keys():
-        max_values[C_V[i][0]] = 0
+for j, jew in enumerate(C_V):
+    jew: Jewelry
+    if jew.category not in max_values.keys():
+        max_values[jew.category] = [j, jew.value]
+    elif jew.value > max_values[jew.category][1]:
+        max_values[jew.category] = [j, jew.value]
 
-for i in range(N):
-    if C_V[i][1] > max_values[C_V[i][0]]:
-        max_values[C_V[i][0]] = C_V[i][1]
+selected_jews = []
 
-
-values = []
-
-for c in max_values.keys():
-    values = sorted(values)
-    if len(values) < 3:
-        values.append(max_values[c])
+for key in max_values:
+    index = max_values[key][0]
+    jew = C_V[index]
+    if len(selected_jews) < M:
+        selected_jews.append(jew)
     else:
-        if max_values[c] > min(values):
-            values[0] = max_values[c]
+        values = [jew.value for jew in selected_jews]
+        if jew.value > min(values):
+            for j, selected_jew in enumerate(selected_jews):
+                if selected_jew.value == min(values):
+                    selected_jews[j] = jew
+
+selected_jews_2 = []
+
+for j, jew in enumerate(C_V):
+    selected_jews_index = [x.index for x in selected_jews]
+    if j in selected_jews_index:
+        continue
+    else:
+        if len(selected_jews_2) < (K - M):
+            selected_jews_2.append(jew)
+        else:
+            values = [jew.value for jew in selected_jews_2]
+            if jew.value > min(values):
+                for j, selected_jew in enumerate(selected_jews_2):
+                    if selected_jew.value == min(values):
+                        selected_jews[j] = jew
 
 
-print(max_values)
-print(values)
-print(sum(values))
+sum_1 = sum([x.value for x in selected_jews])
+sum_2 = sum([x.value for x in selected_jews_2])
+
+print(sum_1+sum_2)
