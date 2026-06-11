36a7156ae78a2f59d440823002f485aa4a516782
gonchiman <legendaryyamasama@gmail.com>
2026-06-12 08:50:20 +0900

    s

--- AtCoder Beginner Contest/ABC/B/455.py
+++ AtCoder Beginner Contest/ABC/B/455.py
@@ -3,10 +3,16 @@ S = [input().split() for _ in range(H)]
 
 
 def is_symmetry(s: list[list]) -> bool:
+    pass
 
 
-
+# i, j は検査マスの大きさ
 for i in range(H):
     for j in range(W):
-        for k in range(int(H/i)):
-            for l in range(int())
+        # k, l は検査回数
+        K = H - i
+        for k in range(K):
+            L = W - j
+            for l in range(L):
+                block = [S[m][l:l+j]for m in range(k, k+i+1)]
+                print(block)
