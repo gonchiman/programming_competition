715a32d260078253ef95f8fa13ca6cece536518f
gonchiman <legendaryyamasama@gmail.com>
2026-06-13 07:05:08 +0900

    s

--- AtCoder Beginner Contest/ABC/B/451.py
+++ AtCoder Beginner Contest/ABC/B/451.py
@@ -2,10 +2,21 @@ N, M = map(int, input().split())
 AB = [list(map(int, input().split())) for _ in range(N)] 
 
 
-current_count = int()
-next_year_count = int()
+# 部署番号：人数
+current_count = dict()
+next_year_count = dict()
+
 for m in range(M):
     m += 1
+    current_count[m] = 0
+    next_year_count[m] = 0
 
+for ab in AB:
+    a, b = ab
+    current_count[a] += 1
+    next_year_count[b] += 1
 
-print(AB)
+for m in range(M):
+    m += 1
+    diff = next_year_count[m] - current_count[m]
+    print(diff)
