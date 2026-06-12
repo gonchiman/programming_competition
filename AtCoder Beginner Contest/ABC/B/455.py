43dd436be94bf9a1d514666a78c383de70469e4c
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-12 14:24:58 +0900

    s

--- AtCoder Beginner Contest/ABC/B/455.py
+++ AtCoder Beginner Contest/ABC/B/455.py
@@ -1,10 +1,18 @@
 H, W = map(int, input().split())
-S = [input().split() for _ in range(H)]
+S = [input() for _ in range(H)]
 
 
-def is_symmetry(s: list[list]) -> bool:
-    pass
+def check_symmetry(block: list) -> bool:
+    for i in range(len(block)):
+        for j in range(len(block[0])):
+            if block[i][j] == block[-i-1][-j-1]:
+                continue
+            else:
+                return False
+    return True
 
+
+count = 0
 
 # i, j は検査マスの大きさ
 for i in range(H):
@@ -14,5 +22,11 @@ for i in range(H):
         for k in range(K):
             L = W - j
             for l in range(L):
-                block = [S[m][l:l+j]for m in range(k, k+i+1)]
-                print(block)
+                block = [S[m][l:l+j+1] if l != l+j else S[m][l] for m in range(k, k+i+1)]
+                if check_symmetry(block):
+                    count += 1
+                # print(f"i, j, k, l = {i}, {j}, {k}, {l}")
+                # print(block)
+                # print()
+
+print(count)
