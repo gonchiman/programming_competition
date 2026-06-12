01245ef306830bfb35d6f49d50f3206efff2cc4f
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-13 06:51:56 +0900

    s

---
+++ AtCoder Beginner Contest/ABC/B/451.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+AB = [list(map(int, input().split())) for _ in range(N)] 
+
+
+current_count = int()
+next_year_count = int()
+for m in range(M):
+    m += 1
+
+
+print(AB)
