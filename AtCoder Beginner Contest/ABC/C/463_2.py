a4ef193b531dbc1da508a7bf96fbc77b288e4a31
Yamamoto <107669839+gonchiman@users.noreply.github.com>
2026-06-21 16:08:08 +0900

    s

--- AtCoder Beginner Contest/ABC/C/463_2.py
+++ AtCoder Beginner Contest/ABC/C/463_2.py
@@ -5,25 +5,23 @@ input = sys.stdin.readline
 
 N = int(input())
 
-H = []
-L = []
+heights = []
+leave_times = []
 
 for _ in range(N):
-    h, l = map(int, input().split())
-    H.append(h)
-    L.append(l)
+    H, L = map(int, input().split())
 
-# suffix_max[i] = i番目以降の身長の最大値
-suffix_max = [0] * (N + 1)
+    while heights and heights[-1] <= H:
+        heights.pop()
+        leave_times.pop()
 
-for i in range(N - 1, -1, -1):
-    suffix_max[i] = max(H[i], suffix_max[i + 1])
+    heights.append(H)
+    leave_times.append(L)
 
 Q = int(input())
-T = list(map(int, input().split()))
 
-for t in T:
-    # L_i <= t の人は退室済み
-    # L_i > t となる最初の位置を探す
-    idx = bisect_right(L, t)
-    print(suffix_max[idx])
+queries = list(map(int, input().split()))
+
+for T in queries:
+    idx = bisect_right(leave_times, T)
+    print(heights[idx])
