4ec199cec7b629a30d1f1d023baca661b4ae9ac9
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-23 11:29:35 +0900

    s

--- AtCoder Beginner Contest/ABC/C/457.py
+++ AtCoder Beginner Contest/ABC/C/457.py
@@ -6,9 +6,12 @@ for _ in range(N):
     A.append("".join(L_A[1:]))
 
 C = list(map(int, input().split()))
-res = ""
-for i, c in enumerate(C):
-    res += A[i] * c
 
+for i in range(N):
+    length = len(A[i]) * C[i]
 
-print(res[K-1])
+    if K > length:
+        K -= length
+    else:
+        print(A[i][(K - 1) % len(A[i])])
+        break
