89562920ef713d16b7923b079f1240c313b0dd3e
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-07-03 17:06:22 +0900

    s

--- AtCoder Beginner Contest/ABC/C/451.py
+++ AtCoder Beginner Contest/ABC/C/451.py
@@ -1,31 +1,12 @@
+from heapq import heappop, heappush
+
+que = []
 q = int(input())
-queries = [list(map(int, input().split())) for _ in range(q)]
-
-
-max_height = 0
-for query in queries:
-    x, y = query
-    if x == 1:
-        if y > max_height:
-            max_height = y
-# print(max_height)
-
-tree_dict_by_height = {}
-for i in range(max_height):
-    i = i + 1
-    tree_dict_by_height[i] = 0
-# print(tree_dict_by_height)
-
-for query in queries:
-    x, y = query
-    if x == 1:
-        tree_dict_by_height[y] += 1
-# print(tree_dict_by_height)
-
-threshold = 0
-for query in queries:
-    x, y = query
-    if x == 2:
-        if y > threshold:
-            threshold = y
-print(threshold)
+for i in range(q):
+    t, h = map(int, input().split())
+    if t == 1:
+        heappush(que, h)
+    else:
+        while que and que[0] <= h:
+            heappop(que)
+    print(len(que))
